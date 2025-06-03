7137c906605f3588553218f97b7788857d2dcafa
PJRojasY <sthongurdt@hotmail.com>
2025-06-03 16:38:47 +0200

    Create exec_docker_tf_res.sh

---
+++ deploy/exec_docker_tf_res.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Activa el modo de depuración: muestra cada comando antes de ejecutarlo
+set -x
+
+# Configura entorno Docker
+g5k-setup-docker -t
+
+# Login en DockerHub
+docker login -u <user> -p 'TOKEN'
+
+# Navegar al directorio del benchmark
+cd ~/mlperf/inference/vision/classification_and_detection/
+
+# Exportar variables de entorno
+export MODEL_DIR=~/mlperf/models/tf
+export DATA_DIR=~/mlperf/data/fake_imagenet/
+
+# Ejecutar pruebas
+./run_and_time.sh tf resnet50 cpu --accuracy
+sleep 120
+./run_and_time.sh tf resnet50 cpu --scenario SingleStream
+sleep 120
+./run_and_time.sh tf resnet50 cpu --scenario MultiStream
+sleep 120
+
+echo "Finalizando la sesión OAR..."
+# Desactiva el modo de depuración si se desea
+set +x
+
+exit 0
